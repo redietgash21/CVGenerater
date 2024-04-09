f9dd770f8caac0f5df6f6c489a096ad44454b79e
redietgash21 <redietgash21@gmail.com>
2024-04-09 12:02:10 +0300

    Updates

---
+++ precache-manifest.c0ce6478d4522ffbc7377aa73ac64242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8f98b53f2d2cad7a56edb2fe4d99e1",
+    "url": "/CVGenerater/index.html"
+  },
+  {
+    "revision": "eeca65b1b2f7d3555fdd",
+    "url": "/CVGenerater/static/css/main.7df8c91f.chunk.css"
+  },
+  {
+    "revision": "f174fabd18729b7e5575",
+    "url": "/CVGenerater/static/js/2.07be4d51.chunk.js"
+  },
+  {
+    "revision": "916864be0487c891bb8f",
+    "url": "/CVGenerater/static/js/3.baddd4f7.chunk.js"
+  },
+  {
+    "revision": "cda12828bdd1b4e667ec",
+    "url": "/CVGenerater/static/js/4.6a80a4cc.chunk.js"
+  },
+  {
+    "revision": "eeca65b1b2f7d3555fdd",
+    "url": "/CVGenerater/static/js/main.18e647e7.chunk.js"
+  },
+  {
+    "revision": "47bd852aa6d39e9b163c",
+    "url": "/CVGenerater/static/js/runtime~main.daa38a13.js"
+  }
+]);
