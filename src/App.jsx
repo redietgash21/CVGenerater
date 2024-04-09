4ccb813e22687afbe227666ff399a24c079439b9
redietgash21 <redietgash21@gmail.com>
2024-04-09 11:49:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+
+
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  // Link,
+} from "react-router-dom"
+import './App.css'
+
+// import Designs from './components/Designs.jsx'
+
+import MainInterface from "./components/MainInterface.jsx"
+import TheForm from "./components/TheForm.jsx"
+import Login from "./components/Login.jsx"
+import ErrorPage from "./components/ErrorPage.jsx"
+import Signup from "./components/Signup.jsx"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:   <MainInterface/>,
+  },
+  {
+    path:"/design",
+    element: <TheForm/>,
+  },
+  {
+    path:"/login",
+    element: <Login/>,
+  },
+  {
+    path:"/signup",
+    element: <Signup/>,
+  },
+  {
+    path:"*",
+    element: <ErrorPage/>
+  }
+])
+
+function App() {
+
+
+  return (
+    <>
+    <RouterProvider router={router}/>
+   
+    </>
+  )
+}
+
+export default App
